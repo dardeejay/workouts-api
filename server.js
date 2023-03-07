f0319079fe59afa8ba555f5c6e433f4b3804edd4
Dardee Jay C. Calura <68296049+dardeejay@users.noreply.github.com>
2023-03-07 11:53:20 +0800

    cors added

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const workoutRoutes = require('./routes/workout')
 const mongoose = require('mongoose')
-
+const cors = require('cors')
 const port = 4000
 
 // express app
@@ -10,7 +10,7 @@ const app = express()
 
 // middleware
 app.use(express.json())
-
+app.use(cors())
 app.use((req, res, next)=>{
     console.log(req.path,req.method)  
     next()
